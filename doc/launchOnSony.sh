156ae599562aa8c253af51e3e6d9cdaa39f6a0da
Martin Mucha <martin.mucha@embedit.cz>
2022-02-25 21:42:44 +0100

    new changes
    Signed-off-by: Martin Mucha <martin.mucha@embedit.cz>

--- doc/launchOnSony.sh
+++ doc/launchOnSony.sh
@@ -1,1 +1,1 @@
-curl -v -X POST "http://192.168.1.99/sony/$1" --header "X-Auth-PSK: pass" --data-binary @-
+curl -s -S -X POST "http://192.168.1.99/sony/$1" --header "X-Auth-PSK: pass" --data-binary @-
